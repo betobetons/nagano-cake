850785a0d8299e1694216ec47319cbea4cb22c33
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-08-18 10:20:16 +0000

    カート機能終わり

--- app/controllers/public/carts_controller.rb
+++ app/controllers/public/carts_controller.rb
@@ -2,27 +2,42 @@ class Public::CartsController < ApplicationController
   def index
     @carts = current_customer.carts
   end
-  
-def create
-  @cart = current_customer.carts.find_by(item_id: cart_params[:item_id])
-  
-  if @cart
-    @cart.quantity += cart_params[:quantity].to_i
-    @cart.save
-    redirect_to carts_path
-  else
-    @cart = current_customer.carts.new(cart_params)
-    @cart.save
-    render "item"
-  end
-end
 
-  
+  def create
+    @cart = current_customer.carts.find_by(item_id: cart_params[:item_id])
+
+    if @cart
+      @cart.quantity += cart_params[:quantity].to_i
+      @cart.save
+      redirect_to carts_path
+    else
+      @cart = current_customer.carts.new(cart_params)
+      @cart.save
+      redirect_to carts_path
+    end
+  end
+
+  def update
+    @cart = Cart.find(params[:id])
+    if @cart.update(cart_params)
+      redirect_to carts_path
+    else
+      redirect_to carts_path
+    end
+  end
+
+  def destroy
+    @cart = Cart.find(params[:id])
+    @cart.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+
   private
-  
+
   def cart_params
      params.require(:cart).permit(:quantity, :item_id)
-    
+
   end
-  
+
 end
